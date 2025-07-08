11a6364d3e060d776229fbf2622edcc464564c2f
William Lindsey <deandrelindsey16@gmail.com>
2025-07-08 10:31:19 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,10 +1,27 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'drug_meth resource'
+author 'Deandre Lindsey'
+description 'Custom Meth System with Processing Stages and Robbery Risks'
 version '1.0.0'
 
-shared_script 'config.lua'
-shared_script 'locales/en.lua'
-client_script 'client/main.lua'
-server_script 'server/main.lua'
+lua54 'yes'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+    'locales/en.lua'
+}
+
+client_scripts {
+    'client/*.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/*.lua'
+}
+
+files {
+    'locales/en.lua'
+}
