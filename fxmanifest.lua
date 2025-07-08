fbc6f1c37b26d69cbb3008e4aeecf134d9093bb8
William Lindsey <deandrelindsey16@gmail.com>
2025-07-08 12:47:39 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,12 +14,12 @@ shared_scripts {
 }
 
 client_scripts {
-    'client/*.lua'
+    'client/main.lua'
 }
 
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
-    'server/*.lua'
+    'server/main.lua'
 }
 
 files {
